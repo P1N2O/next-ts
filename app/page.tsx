c40fcbb4b9349aeaa6482668140786f1e59254ef
Manuel Pinto <manuel@pinto.dev>
2024-01-30 22:01:34 +0530

    Initial commit from Create Next App
    Signed-off-by: Manuel Pinto <manuel@pinto.dev>

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-xl font-bold'>Next.ts</h1>
+      <h2 className='text-base font-normal'>Next.ts starter template</h2>
+    </>
+  )
+}
